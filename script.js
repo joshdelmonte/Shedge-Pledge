f7171a80864b74895bd1bba41ceba1407261a785
joshdelmonte <joshdelmonte@gmail.com>
2022-09-18 11:01:05 -0600

    finish finsish the save buttons

--- script.js
+++ script.js
@@ -148,6 +148,91 @@ function shedgePledge() {
       let savedInput9 = (localStorage.getItem("toDo9"));
       toDoInput9.val(savedInput9);
   
-      
+      // start if else statements to show past, present, future events
+  
+      let now = new Date().getHours();
+      if (now > 8) {
+        $("#input-field-0").addClass("past");
+      } else if (now >= 8 && now < 9) {
+        $("#input-field-0").addClass("present");
+      } else if (now < 8) {
+        $("#input-field-0").addClass("future");
+      }
+  
+      if (now > 9) {
+        $("#input-field-1").addClass("past");
+      } else if (now >= 9 && now < 10) {
+        $("#input-field-1").addClass("present");
+      } else if (now < 9) {
+        $("#input-field-1").addClass("future");
+      }
+  
+      if (now > 10) {
+        $("#input-field-2").addClass("past");
+      } else if (now >= 10 && now < 11) {
+        $("#input-field-2").addClass("present");
+      } else if (now < 10) {
+        $("#input-field-2").addClass("future");
+      }
+  
+      if (now > 11) {
+        $("#input-field-3").addClass("past");
+      } else if (now >= 11 && now < 12) {
+        $("#input-field-3").addClass("present");
+      } else if (now < 11) {
+        $("#input-field-3").addClass("future");
+      }
+  
+      if (now > 12) {
+        $("#input-field-4").addClass("past");
+      } else if (now >= 12 && now < 13) {
+        $("#input-field-4").addClass("present");
+      } else if (now < 12) {
+        $("#input-field-4").addClass("future");
+      }
+  
+      if (now > 13) {
+        $("#input-field-5").addClass("past");
+      } else if (now >= 13 && now < 14) {
+        $("#input-field-5").addClass("present");
+      } else if (now < 13) {
+        $("#input-field-5").addClass("future");
+      }
+  
+      if (now > 14) {
+        $("#input-field-6").addClass("past");
+      } else if (now >= 14 && now < 15) {
+        $("#input-field-6").addClass("present");
+      } else if (now < 14) {
+        $("#input-field-6").addClass("future");
+      }
+  
+      if (now > 15) {
+        $("#input-field-7").addClass("past");
+      } else if (now >= 15 && now < 16) {
+        $("#input-field-7").addClass("present");
+      } else if (now < 15) {
+        $("#input-field-7").addClass("future");
+      }
+  
+      if (now > 16) {
+        $("#input-field-8").addClass("past");
+      } else if (now >= 16 && now < 17) {
+        $("#input-field-8").addClass("present");
+      } else if (now < 16) {
+        $("#input-field-8").addClass("future");
+      }
+  
+      if (now > 17) {
+        $("#input-field-9").addClass("past");
+      } else if (now >= 17 && now < 18) {
+        $("#input-field-9").addClass("present");
+      } else if (now < 17) {
+        $("#input-field-9").addClass("future");
+      }
+  
+    });
+  
+  }
   shedgePledge();
   
