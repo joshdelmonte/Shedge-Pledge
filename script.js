7fe4b54fe2800f9ff520e5b2091111a31e653e11
joshdelmonte <joshdelmonte@gmail.com>
2022-09-18 12:48:03 -0600

    A little augmentation to some text

--- script.js
+++ script.js
@@ -8,17 +8,17 @@
 function shedgePledge() {
     $(document).ready(function () {
       let momentVar = moment().format('dddd, MMMM Do, YYYY h:mm a');
-      $("#currentDay").append(currentDay);
+      $("#currentDay").append(diaHoje);
       let workHours = ["9 a.m.", "10 a.m.", "11 a.m.", "12 p.m.", "1 p.m.", "2 p.m.", "3 p.m.", "4 p.m.",
         "5 p.m."]
   
       //THEN the current day is displayed at the top of the calendar
-      function currentDay() {
-        let currentDay = $("#currentDay").text(momentVar);
-        return currentDay;
+      function diaHoje() {
+        let diaHoje = $("#currentDay").text(momentVar);
+        return diaHoje;
       }
   
-      currentDay();
+      diaHoje();
     
   
       for (i = 0; i < workHours.length; i++) {
